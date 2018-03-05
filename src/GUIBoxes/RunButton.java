7a99e75ccf031741c83943a3a22d44d42ac7d024
Calvin Ma <cm298@duke.edu>
2018-03-05 15:19:15 -0500

    language gets npe

--- src/GUIBoxes/RunButton.java
+++ src/GUIBoxes/RunButton.java
@@ -14,34 +14,32 @@ public class RunButton extends Buttons {
 
 	private static TextInputBox mainTextInput;
 	private static PrevCommandList mainPrevCommandBox;
-	private static LanguageCombo mainLanguageComboBox;
+	private static GUIComboBox mainLanguageComboBox;
 	private static String language = "English";
-	
+
 	private Map<String, Double> variableMap;
 
 	public RunButton(Group root, GUIComboBox languageComboBox, double[] properties, String text, TextInputBox textInput,
 			PrevCommandList prevCommandBox, ArrayList<Turtle> turtleList, Map<String, Double> variables) {
 		super(root, properties, text, turtleList);
-		mainLanguageComboBox = (LanguageCombo) languageComboBox;
+		mainLanguageComboBox = languageComboBox;
 		this.mainTextInput = textInput;
 		this.mainPrevCommandBox = prevCommandBox;
 		variableMap = variables;
 	}
-	
 
 	@Override
 	void setupAction() {
-		
+
 		getButton().setOnAction((event) -> {
 			mainPrevCommandBox.addText(mainTextInput.getText());
 			for (Turtle t : getThisTurtleList()) {
 				System.out.println(mainTextInput.getText());
-				if (!language.equals("English")) {
-					language = mainLanguageComboBox.getLanguage();
-					System.out.println("here "+ language + mainLanguageComboBox.getLanguage());
-				}
+				language = ((LanguageCombo) mainLanguageComboBox).getLanguage();
+				System.out.println("here " + language + ((LanguageCombo) mainLanguageComboBox).getLanguage());
+
 				System.out.println("crap");
-				System.out.println("test  " + mainLanguageComboBox.getLanguage());
+				System.out.println("test  " + ((LanguageCombo) mainLanguageComboBox).getLanguage());
 				Command test = new Command(mainTextInput.getText(), t, variableMap, language);
 				test.execute();
 			}
--- src/main/Main.java
+++ src/main/Main.java
@@ -40,7 +40,7 @@ public class Main extends Application {
 	private static String title;
 	private final static double SCREEN_HEIGHT = 600;
 	private final static double SCREEN_WIDTH = 1215;//915;
-	private static Stage stage;
+	private static Stage myStage;
 	private static TextInputBox textInput;
 	private static ScreenBox turtleScreen;
 	private static GUIComboBox languageComboBox;
@@ -89,8 +89,8 @@ public class Main extends Application {
 		myScene = new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT, BACKGROUND);
 		setStage();
 		setupGUIBoxes();
-		setupButtons();
 		setupComboboxes();
+		setupButtons();
 		setupTurtleCheckbox();
 		
 	}
@@ -117,7 +117,6 @@ public class Main extends Application {
 	private void setupComboboxes() {
 		new BackgroundCombo(root, turtleScreen, GUIProperties.get("backgroundCombo"), "Change Background Color");
 		languageComboBox = new LanguageCombo(root, GUIProperties.get("languageCombo"), "Change Language");
-		((LanguageCombo) languageComboBox).getLanguage();
 		new PenCombo(root, turtleList, GUIProperties.get("penCombo"), "Change Pen Color");
 
 	}
