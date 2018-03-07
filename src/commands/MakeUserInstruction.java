f3746ce07bb99b7d1d9ce4516e94f5dc996433d8
Miles Todzo <mt265@duke.edu>
2018-03-06 20:35:13 -0500

    added toString for userInstruction

--- src/commands/MakeUserInstruction.java
+++ src/commands/MakeUserInstruction.java
@@ -9,22 +9,10 @@ public class MakeUserInstruction extends Command{
 	private String myCommandText;
 	private List<String> myParameters;
 	public MakeUserInstruction(List<String> parameters, String commands) {
-//		paramToValueMap = new HashMap<>();
 		myCommandText = commands;
 		myParameters = parameters;
-//		for (String param: parameters) {
-//			System.out.println("hm "+ param);
-//			paramToValueMap.put(param, null);
-//			System.out.println("no");
-//		}
-		System.out.println("bye");
 	}
-	
-//	@Override
-//	public double execute(List<CommandNode> children, Turtle t) {
-//		
-//	}
-	
+		
 	public String getInstructionText(List<String> paramValues) {
 		String paramsInserted = myCommandText;
 		for (int x=0; x<paramValues.size(); x++) {
@@ -33,7 +21,8 @@ public class MakeUserInstruction extends Command{
 		return paramsInserted;
 	}
 	
-//	public void setParamValue(String param, double value) {
-//		paramToValueMap.put(param, value);
-//	}
+	@Override
+	public String toString() {
+		return myCommandText;
+	}
 }
