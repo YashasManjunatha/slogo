b218a76ce7547967478c4ab2c58294417d7e5164
Stefani Vukajlovic <sv116@duke.edu>
2018-03-09 20:55:42 -0500

    AskWith

--- src/commands/Tell.java
+++ src/commands/Tell.java
@@ -14,18 +14,11 @@ public class Tell extends Command{
 	@Override
 	double execute(List<CommandNode> children, Turtle t) {
 		double retVal = 0;
-		CommandNode bracketNode1 = children.get(0);
-		CommandNode bracketNode2 = children.get(1);
-		List<CommandNode> bracket1children = bracketNode1.getChildren();
+		//check if the turtle is active and if its id is present, 
+		//if not active but id here, set active, otherwise inactive
 		
-		List<Double> IDs= new ArrayList<>();
-		
-		for(int i=0; i<bracket1children.size();i++) {
-		  IDs.add(bracket1children.get(i).execute(t));
-		}
-		if(IDs.contains(3)) { //t.getID()
-			retVal =bracketNode2.execute(t);
-		}
+		//also need the list of all existing ids, if the id does not exist, 
+		//make new turtle
 		return retVal;
 	}
 
